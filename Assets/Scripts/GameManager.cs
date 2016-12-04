3e2eb026a7e93f1b27ee0e5f5d5ef076a565f15e
nelson junior <ircnelson@gmail.com>
2016-12-04 03:09:43 -0300

    Register player in game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private const string PLAYER_ID_PREFIX = "Player ";
+    private static Dictionary<string, Player> players = new Dictionary<string, Player>();
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("More than one GameManager in scene.");
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    public static void RegisterPlayer(string netId, Player player)
+    {
+        string playerId = PLAYER_ID_PREFIX + netId;
+
+        players.Add(playerId, player);
+
+        player.transform.name = playerId;
+    }
+
+    public static void UnRegisterPlayer(string playerId)
+    {
+        players.Remove(playerId);
+    }
+
+    public static Player GetPlayer(string playerId)
+    {
+        return players[playerId];
+    }
+
+    public static Player[] GetAllPlayers()
+    {
+        return players.Values.ToArray();
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour
+{
+    [SyncVar]
+    public string nickname;
+
+    public override void OnStartClient()
+    {
+        Debug.Log("OnStartClient");
+
+        base.OnStartClient();
+
+        var netId = GetComponent<NetworkIdentity>().netId.ToString();
+        var player = GetComponent<Player>();
+
+        GameManager.RegisterPlayer(netId, player);
+    }
+
+    private void Start()
+    {
+        Debug.Log("Start");
+        
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        Debug.Log("OnStartLocalPlayer");
+
+        GetComponent<MeshRenderer>().material.color = Color.green;
+
+        CmdPlayerJoined(transform.name, User.instance.Nickname);
+    }
+    
+    [Command]
+    private void CmdPlayerJoined(string playerId, string nickname)
+    {
+        var player = GameManager.GetPlayer(playerId);
+        
+        if (player != null)
+        {
+            player.nickname = nickname;
+
+            Debug.Log(nickname + " has joined.");
+        } 
+    }
+
+    private void OnDisable()
+    {
+        GameManager.UnRegisterPlayer(transform.name);
+    }
+}
